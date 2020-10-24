31d654b33344ac142c8ca7ad5c9b05e0ceccf8a5
TaccaT17 <tom@crammond.name>
2020-10-23 21:37:20 -0700

    Created UserStats class

---
+++ DiscordUserStatsBot/UserStats.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace DiscordUserStatsBot
+{
+    class UserStats
+    {
+        //Will log stats up to a year
+
+        //VARIABLES START
+        //---------------------------------------------------------------------------------------------------------------------------------------------------------------
+        private float totalVoiceChatTime;
+        private int totalMessagesSent;
+
+        public int TotalMessagesSent { get; }
+
+        //---------------------------------------------------------------------------------------------------------------------------------------------------------------
+        //VARIABLES END
+
+        //FUNCTION START
+        //---------------------------------------------------------------------------------------------------------------------------------------------------------------
+        public void UpdateUserStats()
+        {
+
+        }
+        
+        public string GetTotalVoiceChatTime()
+        {
+            string totalVoiceChatTime = "00:00:00";
+
+            return totalVoiceChatTime;
+        }
+        //---------------------------------------------------------------------------------------------------------------------------------------------------------------
+        //FUNCTION END
+
+    }
+}
